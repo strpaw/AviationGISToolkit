edb7a2cca37be9d0d17443cbd21b1d9ed769e029
strpaw <38607111+strpaw@users.noreply.github.com>
2020-04-27 17:57:56 +0200

    Added handling of DMS separated format

--- aviation_gis_toolkit/angle_base.py
+++ aviation_gis_toolkit/angle_base.py
@@ -1,5 +1,5 @@
-from aviation_gis_toolkit.base_tools import BasicTools
-from aviation_gis_toolkit.const import *
+from .base_tools import BasicTools
+from .const import *
 import re
 
 
@@ -48,6 +48,19 @@ ANGLE_PATTERNS = {
                 (?P<hem_suffix>[EW]?)  # Hemisphere suffix
                 $  # End of the string
                ''', re.VERBOSE),
+        'DMS_SEP': re.compile(r'''
+                ^  # Start of the string
+                (?P<hem_prefix>[-+EW]?)  # Hemisphere prefix
+                (\s)?
+                (?P<deg>\d{1,3})  # Degrees
+                (\W)
+                (?P<min>\d{1,2})  # Minutes
+                (\W)
+                (?P<sec>\d{1,2}(\.\d+)?)  # Seconds
+                (\W)?
+                (?P<hem_suffix>[EW]?)  # Hemisphere suffix
+                $  # End of the string
+               ''', re.VERBOSE),
     },
     AT_LAT: {
         'DMS_COMP': re.compile(r'''
@@ -73,6 +86,8 @@ ANGLE_PATTERNS = {
 
 class AngleBase(BasicTools):
 
+    DIRECTION_NEGATIVE = ['-', 'S', 'W']
+
     def __init__(self, ang_src=None, ang_type=None):
         BasicTools.__init__(self)
         self.ang_src = ang_src
@@ -89,7 +104,7 @@ class AngleBase(BasicTools):
             s = float(parts.group('sec'))
             h = parts.group('hem_prefix') + parts.group('hem_suffix')
             dd = d + m / 60 + s / 3600
-            if h in ['W', 'S', '-']:
+            if h in AngleBase.DIRECTION_NEGATIVE:
                 dd = -dd
             return dd
 
@@ -102,7 +117,7 @@ class AngleBase(BasicTools):
             m = float(parts.group('min'))
             h = parts.group('hem_prefix') + parts.group('hem_suffix')
             dd = d + m / 60
-            if h in ['W', 'S', '-']:
+            if h in AngleBase.DIRECTION_NEGATIVE:
                 dd = -dd
             return dd
 
@@ -112,11 +127,12 @@ class AngleBase(BasicTools):
             if ang_type_patterns[pattern].match(ang):
                 print(pattern)
                 parts = ang_type_patterns[pattern].search(ang)
-                if pattern in ['DMS_COMP', 'DMS_SEP_SPACE', 'DMS_SEP_HYPHEN']:
+                if pattern in ['DMS_COMP', 'DMS_SEP_SPACE', 'DMS_SEP_HYPHEN', 'DMS_SEP']:
                     return self.dms_groups_to_dd(parts)
                 elif pattern == 'DM_COMP':
                     return self.dm_groups_to_dd(parts)
 
     def angle_to_dd(self, ang):
+        ang_norm = AngleBase.get_normalized_src_value(ang)
         dd = self.compacted_format_to_dd(ang)
         return dd
