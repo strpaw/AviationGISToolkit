d4fcd4eac1181cd2e56a564f7410913dd23a9be5
strpaw <38607111+strpaw@users.noreply.github.com>
2020-05-18 11:56:24 +0200

    Convert bearing from DD to DMS format

--- aviation_gis_toolkit/bearing.py
+++ aviation_gis_toolkit/bearing.py
@@ -3,6 +3,7 @@ bearing.py
 bearing module provides functionality to deal with bearings: conversion, validation
 """
 import re
+import math
 
 # Bearing regular expressions
 BRNG_REGEX = {
@@ -26,3 +27,41 @@ BRNG_REGEX = {
 # Formatting bearing in degrees, minutes, seconds separated format
 BRNG_STRING_FORMAT = '{d:03d} {m:02d} {s:0{sec_length}.{sec_prec}f}'
 
+
+class Bearing:
+    """ Class covers actions related to bearing: validation, conversion, reduction to azimuth."""
+
+    @staticmethod
+    def _dd_to_dms_parts(ang_dd, prec=3):
+        """ Converts angle given in DD format into DMS format in 'parts', e. g. DD, MM, SEC
+        :param ang_dd: float, angle in DD format
+        :param prec: int, positive number of decimal point of seconds, default value is 3
+        :return tuple: tuple of dd, mm, sec - float
+        """
+
+        d_frac_part, d_whole_part = math.modf(math.fabs(ang_dd))  # frac_part - fractional part
+        m_frac_part, m_whole_part = math.modf(d_frac_part * 60)
+        s_part = m_frac_part * 60
+
+        def sign(a_dd): return 1 if a_dd >= 0 else -1
+        dd = int(d_whole_part)
+        mm = int(m_whole_part)
+        sec = round(s_part, prec)
+
+        return sign(ang_dd), dd, mm, sec
+
+    @staticmethod
+    def bearing_dd_to_dms(brng, prec=3):
+        """ Converts bearing from decimal degrees format to degrees, minutes, seconds space separated format.
+        :param brng: float, bearing in DD format.
+        :param prec: int, positive number of decimal point of seconds
+        :return: str: bearing in DMS format.
+        """
+        sign, d, m, s = Bearing._dd_to_dms_parts(brng, prec)
+
+        sec_length = 2
+        if prec > 0:
+            sec_length = prec + 3
+
+        dms = BRNG_STRING_FORMAT.format(d=d, m=m, s=s, sec_length=sec_length, sec_prec=prec)
+        return dms
