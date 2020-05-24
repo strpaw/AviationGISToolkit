53f01f1a1f5d0e9d2e08cbf6086936e3bc34f6d3
strpaw <38607111+strpaw@users.noreply.github.com>
2020-05-24 15:10:25 +0200

    Refactor: Use conversion methdo from DD into DMS format from AngleBase class

--- aviation_gis_toolkit/bearing.py
+++ aviation_gis_toolkit/bearing.py
@@ -1,14 +1,14 @@
 """
 bearing module provides functionality to deal with bearings: conversion, validation
 """
-from aviation_gis_toolkit.base_tools import BasicTools
+from aviation_gis_toolkit.angle_base import *
 import math
 
 # Formatting bearing in degrees, minutes, seconds separated format
 BRNG_STRING_FORMAT = '{d:03d} {m:02d} {s:0{sec_length}.{sec_prec}f}'
 
 
-class Bearing(BasicTools):
+class Bearing(AngleBase):
     """ Class covers actions related to bearing: validation, conversion, reduction to azimuth.
         Attributes:
     -----------
@@ -30,73 +30,12 @@ class Bearing(BasicTools):
         self.is_valid = None
         self.err_msg = ''
 
-    @staticmethod
-    def _dd_to_dms_parts(ang_dd, prec=3):
-        """ Converts angle given in DD format into DMS format in 'parts', e. g. DD, MM, SEC
-        :param ang_dd: float, angle in DD format
-        :param prec: int, positive number of decimal point of seconds, default value is 3
-        :return tuple: tuple of dd, mm, sec - float
-        """
-
-        d_frac_part, d_whole_part = math.modf(math.fabs(ang_dd))  # frac_part - fractional part
-        m_frac_part, m_whole_part = math.modf(d_frac_part * 60)
-        s_part = m_frac_part * 60
-
-        def sign(a_dd): return 1 if a_dd >= 0 else -1
-        dd = int(d_whole_part)
-        mm = int(m_whole_part)
-        sec = round(s_part, prec)
-
-        return sign(ang_dd), dd, mm, sec
-
-    @staticmethod
-    def bearing_dd_to_dms(brng, prec=3):
+    def get_bearing_dms(self, prec=3):
         """ Converts bearing from decimal degrees format to degrees, minutes, seconds space separated format.
-        :param brng: float, bearing in DD format.
         :param prec: int, positive number of decimal point of seconds
         :return: str: bearing in DMS format.
         """
-        sign, d, m, s = Bearing._dd_to_dms_parts(brng, prec)
-
-        sec_length = 2
-        if prec > 0:
-            sec_length = prec + 3
-
-        dms = BRNG_STRING_FORMAT.format(d=d, m=m, s=s, sec_length=sec_length, sec_prec=prec)
-        return dms
-
-    @staticmethod
-    def _get_dms_parts(brng):
-        """ Gets degrees, minutes, seconds from bearing in DMS format.
-        :param brng: str, bearing in DMS format (separated or compacted).
-        :return: tuple:
-        """
-        for brng_regex in BRNG_REGEXS.values():
-            mo = brng_regex.match(brng)  # Check if there is matching object for given pattern
-            if mo:  # If it is - get parts of bearing in DMS format
-                d = int(mo.group('deg'))
-                m = int(mo.group('min'))
-                s = float(mo.group('sec'))
-                return d, m, s
-
-    @staticmethod
-    def _dms_parts_to_dd(parts):
-        """ Converts dms parts to float value of DMS format
-        :param parts: tuple
-        :return: float: DMS bearing in DD format
-        """
-        d, m, s = parts
-        if m < 60 and s < 60:
-            return d + m / 60 + s / 3600
-
-    @staticmethod
-    def _bearing_dms_to_dd(brng):
-        """ Converts bearing from DMS format to DD format.
-        :param brng: str, normalized source bearing value
-        :return: float: bearing in DD format, None if it is wrong source bearing value
-        """
-        parts = Bearing._get_dms_parts(brng)
-        return Bearing._dms_parts_to_dd(parts)
+        return self._dd_to_dms(BRNG_STRING_FORMAT, self.brng_dd, prec)
 
     def bearing_to_dd(self):
         """ Converts bearing from source value to DD format. """
